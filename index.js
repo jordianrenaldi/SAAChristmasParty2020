5f0db697afbf0984f349d05a67295695b8793dec
jordianrenaldi <60659786+jordianrenaldi@users.noreply.github.com>
2020-12-20 00:53:53 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -56,12 +56,12 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "CG Christmas has started, BLESS YOUR SOUL IF YOU ARE LATE.";
+    document.getElementById("timer").innerHTML = "CG Christmas has started, BLESS YOUR SOUL IF YOU ARE LATE.";
   }
 }, 1000);
